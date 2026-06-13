f48ee0830bf33daf9d78cfc23bcd629708b1b847
nqlahh <nuraqilahtan80@gmail.com>
2026-06-13 23:54:30 +0800

    Delete splitapp directory

--- splitapp/splitapp/src/main/java/com/splitapp/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.splitapp.repository;
-
-import com.splitapp.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    List<Expense> findByGroupId(Long groupId);
-}
--- splitapp/splitapp/src/main/java/com/splitapp/repository/GroupRepository.java
+++
@@ -1,6 +0,0 @@
-package com.splitapp.repository;
-
-import com.splitapp.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> { }
